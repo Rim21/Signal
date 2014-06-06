a8f7e832a48b227445c958ebececd73b101ed381
Rim21 <nathamir9@gmail.com>
2014-06-07 08:21:30 +1000

    Feb_Changes
    Finally figured out that the approach in the coding would not work for
    this particular case. The learning experience however was great

--- sketch_feb11a_with_90usec_delay/sketch_feb11a_with_90usec_delay.ino
+++ sketch_feb11a_with_90usec_delay/sketch_feb11a_with_90usec_delay.ino
@@ -1,14 +1,19 @@
 /* 3 Channel Signal Calibration
 
-This item is the baseline to feb_09b. Excludes the DigitalIOPerformance.
+This item is rolled over from feb_09a&c, feb_08a which is
+also based on feb_05a.
 
-Result = same as for feb_09b - no performance gain?
+Motor does not run.
 
-Include the pinchangeint library to access 3 x pin interrupts
+ include the pinchangeint library to access 3 x pin interrupts
 */
 #include <PinChangeInt.h>
 // include the elapsedMillis library to access 3 x micros timers
 #include <elapsedMillis.h>
+// include the digitalIOPerformance library for Leo Board and to
+// access fast digital write and digital read. Pseudo equivalent
+// to direct port manipulation. Must use const for pins.
+#include <digitalIOPerformance.h> 
 
 // Assign Leo interrupts
 const byte phaseOin = 8;
@@ -30,7 +35,7 @@ volatile unsigned long phaseYintervalin;
 
 //Setup calibration & debounce constants
 //const int debounce = 100;// not used as yet
-float calPercent = .98;
+float calPercent = .99;
 
 // Setup the 3 timers to be used for calibration
 elapsedMicros timerO;
@@ -68,11 +73,19 @@ volatile uint8_t FlagsShared;
   
 void setup()
 {
-//  delay(2500);//let the external circuit settle so as to set the o/p pins to reflect the i/ps
+  //delay(2500);//let the external circuit settle so as to set the o/p pins to reflect the i/ps
 
   pinMode(phaseOout, OUTPUT);    
   pinMode(phaseOin, INPUT);  
+  pinMode(phaseGout, OUTPUT);    
+  pinMode(phaseGin, INPUT);
+  pinMode(phaseYout, OUTPUT);    
+  pinMode(phaseYin, INPUT);
+  
   digitalWrite (phaseOin, HIGH); // turn on the internal pull-up resistor
+  digitalWrite (phaseGin, HIGH);
+  digitalWrite (phaseYin, HIGH); 
+
     if (digitalRead(phaseOin)==HIGH){
     digitalWrite(phaseOout,HIGH);
   }else{
@@ -81,9 +94,7 @@ void setup()
   //digitalWrite(phaseOout, digitalRead(phaseOin)); //gets rid of the multi pulses on i/p
 
   //PhaseG pin setup                      
-  pinMode(phaseGout, OUTPUT);    
-  pinMode(phaseGin, INPUT);
-  digitalWrite (phaseGin, HIGH);
+
     if (digitalRead(phaseGin)==HIGH){
     digitalWrite(phaseGout,HIGH);
   }else{
@@ -92,15 +103,14 @@ void setup()
   //digitalWrite(phaseGout, digitalRead(phaseGin));
   
   //PhaseY pin setup                      
-  pinMode(phaseYout, OUTPUT);    
-  pinMode(phaseYin, INPUT);
-  digitalWrite (phaseYin, HIGH); 
+
     if (digitalRead(phaseYin)==HIGH){
     digitalWrite(phaseYout,HIGH);
   }else{
     digitalWrite(phaseYout,LOW);
   }
-  //digitalWrite(phaseYout, digitalRead(phaseYin));
+  //digitalWrite(phaseYout, digitalRead(phaseYin));//doesn't use the digitalIOPer
+  //macro if written this way as the state is a variable number
   
   // use the PinChangeInt library to attach the interrupts
   // Pins chosen make this interchangeable with an Uno and Leo
@@ -134,20 +144,20 @@ void loop()
     if(UpdateFlags & phaseOflag)
     {
       phaseOinterval = phaseOintervalin;
-      phaseOcal = (phaseOinterval/calPercent); //timer period to wait
+      phaseOcal = (phaseOinterval); //timer period to wait
       
     }
 
     if(UpdateFlags & phaseGflag)
     {
       phaseGinterval = phaseGintervalin;
-      phaseGcal = (phaseGinterval/calPercent);
+      phaseGcal = (phaseGinterval);
     }
 
     if(UpdateFlags & phaseYflag)
     {
       phaseYinterval = phaseYintervalin;
-      phaseYcal = (phaseYinterval/calPercent);
+      phaseYcal = (phaseYinterval);
     }
                 
     // clear shared copy of updated flags
@@ -160,37 +170,34 @@ void loop()
         
     if(timerO > phaseOcal)
     {
+      timerO -= phaseOcal; //reset timer and maintain sync
       if (digitalRead(phaseOin)==HIGH){
          digitalWrite(phaseOout,HIGH);
          }else{
          digitalWrite(phaseOout,LOW);
         }      
-//      digitalWrite(phaseOout, digitalRead(phaseOin));
-      timerO -= phaseOcal; //reset timer and maintain sync
     }
     
 
     if(timerG > phaseGcal)
     {
+      timerG -= phaseGcal; //reset the timer
       if (digitalRead(phaseGin)==HIGH){
          digitalWrite(phaseGout,HIGH);
          }else{
          digitalWrite(phaseGout,LOW);
-        }      
-//      digitalWrite(phaseGout, digitalRead(phaseGin));
-      timerG -= phaseGcal; //reset the timer
+        }            
     }
   
 
     if(timerY > phaseYcal)
     {
+      timerY -= phaseYcal; //reset the timer
       if (digitalRead(phaseYin)==HIGH){
          digitalWrite(phaseYout,HIGH);
          }else{
          digitalWrite(phaseYout,LOW);
-        }      
-//      digitalWrite(phaseYout, digitalRead(phaseYin));
-      timerY -= phaseYcal; //reset the timer
+        }            
     }
  
   UpdateFlags = 0; 
